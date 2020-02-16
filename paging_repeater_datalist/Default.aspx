3fbcdb5faaa9c56eaee87767721822eb240f0ebc
poojaztreasury <59700485+poojaztreasury@users.noreply.github.com>
2020-02-16 06:22:12 -0800

    testing

---
+++ paging_repeater_datalist/Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+            height: 85px;
+        }
+        .auto-style4 {
+            width: 551px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style4">
+                        <asp:DataList ID="DataList1" runat="server"  BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2" GridLines="Both" DataKeyField="eid" Width="480px">
+                            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+                            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+                            <ItemStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+                            <ItemTemplate>
+  <b>Emp Id</b><%#Eval("eid") %><br /><b>Emp Name</b><%#Eval("ename") %><br /><b>Department</b><%#Eval("edept") %><br /><b>Salary</b><%#Eval("esalary") %><br /><b>Add this emp on list</b>
+                             
+                            </ItemTemplate>
+                            <SelectedItemStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+                        </asp:DataList>
+                        
+                       
+                        
+                        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:Label ID="Lbpageno" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <asp:Button ID="Button1" runat="server" Text="First"  OnClick="Button1_Click"/>
+&nbsp;<asp:Button ID="Button2" runat="server" Text="Previous" OnClick="Button2_Click" />
+&nbsp;<asp:Button ID="Button3" runat="server" Text="Next"  OnClick="Button3_Click"/>
+&nbsp;<asp:Button ID="Button4" runat="server" Text="Last"  OnClick="Button4_Click"/>
+                        
+                       
+                        
+                    </td>
+                   
+                </tr>
+            </table>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ paging_repeater_datalist/Default.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    Int32 CurrentPage;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (Page.IsPostBack == false)
+        {
+            list_bind();
+            ViewState["PageCount1"] = 0;
+
+        }
+
+        CurrentPage = Convert.ToInt32(ViewState["PageCount1"]);
+
+    }
+    private void list_bind()
+    {
+        SqlDataAdapter adp = new SqlDataAdapter("select * from tbemp", ConfigurationManager.ConnectionStrings["tbtempConnectionString"].ConnectionString);
+        DataTable ds = new DataTable();
+        adp.Fill(ds);
+        // DataList1.DataSource = ds;
+       //  DataList1.DataBind();
+        DataListPaging(ds);
+    }
+
+    private void DataListPaging(DataTable ds)
+    {
+        //creating object of PagedDataSource;  
+
+
+        PagedDataSource PD = new PagedDataSource();
+
+        PD.DataSource = ds.DefaultView;
+        PD.PageSize = 4;
+        PD.AllowPaging = true;
+        PD.CurrentPageIndex = CurrentPage;
+        Button1.Enabled = !PD.IsFirstPage;
+        Button2.Enabled = !PD.IsFirstPage;
+        Button4.Enabled = !PD.IsLastPage;
+        Button3.Enabled = !PD.IsLastPage;
+        ViewState["TotalCount"] = PD.PageCount;
+        DataList1.DataSource = PD;
+        DataList1.DataBind();
+        ViewState["PagedDataSurce"] = ds;
+        Lbpageno.Text = "Page" + (CurrentPage + 1).ToString() + "of" + (ViewState["TotalCount"]).ToString();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        CurrentPage = 0;
+        DataList1.DataSource = (DataTable)ViewState["PagedDataSurce"];
+        DataListPaging((DataTable)ViewState["PagedDataSurce"]);
+
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        CurrentPage = (int)ViewState["PageCount1"];
+        CurrentPage -= 1;
+        ViewState["PageCount1"] = CurrentPage;
+
+        DataListPaging((DataTable)ViewState["PagedDataSurce"]);
+
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        CurrentPage = (int)ViewState["PageCount1"];
+        CurrentPage += 1;
+        ViewState["PageCount1"] = CurrentPage;
+        DataListPaging((DataTable)ViewState["PagedDataSurce"]);
+
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        CurrentPage = (int)ViewState["TotalCount"] - 1;
+        DataListPaging((DataTable)ViewState["PagedDataSurce"]);
+
+    }
+
+
+
+
+    /* protected void Button1_Click(object sender, EventArgs e)
+     {
+         string d = "";
+         for(Int32 i=0; i<DataList1.Items.Count;i++)
+         {
+             CheckBox ch;
+             ch = (CheckBox)(DataList1.Items[i].FindControl("chk"));
+             if(ch.Checked)
+             {
+                 d += DataList1.DataKeys[i].ToString()+ ",";
+             }
+
+         }
+         d = d.Substring(0, d.Length - 1);
+         string qry = "select * from tbemp where eid in(" + d + ")";
+         SqlDataAdapter adp = new SqlDataAdapter(qry, ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+         DataSet ds = new DataSet();
+         adp.Fill(ds);
+         Repeater1.DataSource = ds;
+         Repeater1.DataBind();
+     } */
+
+    /* protected void chk_CheckedChanged(object sender, EventArgs e)
+     {
+         string d = "";
+         for (Int32 i = 0; i < DataList1.Items.Count; i++)
+         {
+             CheckBox ch;
+             ch = (CheckBox)(DataList1.Items[i].FindControl("chk"));
+             if (ch.Checked)
+             {
+                 d += DataList1.DataKeys[i].ToString() + ",";
+             }
+
+         }
+         d = d.Substring(0, d.Length - 1);
+         string qry = "select * from tbemp where eid in(" + d + ")";
+         SqlDataAdapter adp = new SqlDataAdapter(qry, ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+         DataSet ds = new DataSet();
+         adp.Fill(ds);
+         Repeater1.DataSource = ds;
+         Repeater1.DataBind();
+     }*/
+}
